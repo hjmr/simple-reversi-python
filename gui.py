908bcda29cb71d8c6d4bf607616243944092389f
hjmr <2620990+hjmr@users.noreply.github.com>
2021-05-21 03:17:16 +0900

    add simple GUI

---
+++ gui.py
@@ -0,0 +1,67 @@
+import sys
+import pygame
+from pygame.locals import *
+
+import stone
+from game import Game
+from player import Player
+from board import SIZE
+
+WIN_SIZE = (WIN_W, WIN_H) = (600, 600)
+CELL_W, CELL_H = (WIN_W / SIZE, WIN_H / SIZE)
+STONE_SIZE = (CELL_W / 2) * 0.9
+
+COL_GREEN = (0, 150, 0)
+COL_BLACK = (0, 0, 0)
+COL_WHITE = (255, 255, 255)
+
+
+class HumanGUIPlayer(Player):
+    def __init__(self, my_stone):
+        super().__init__(my_stone)
+
+    def next_move(self, board):
+        _done = False
+        _pos = None
+        while not _done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    _pos = (int(event.pos[0] / CELL_W) + 1, int(event.pos[1] / CELL_H) + 1)
+                    _done = True
+        return _pos
+
+    def __str__(self):
+        return "Human"
+
+
+class GUIGame(Game):
+    def __init__(self, players):
+        super().__init__(players)
+        pygame.init()
+        pygame.display.set_caption("Reversi")
+        self.screen = pygame.display.set_mode(WIN_SIZE)
+
+    def show_board(self):
+        self.screen.fill(COL_GREEN)
+        for x in range(SIZE):
+            pygame.draw.line(self.screen, COL_BLACK, (x * CELL_W, 0),  (x * CELL_W, WIN_H))
+        for y in range(SIZE):
+            pygame.draw.line(self.screen, COL_BLACK, (0, y * CELL_H), (WIN_W, y * CELL_H))
+        for x in range(SIZE):
+            for y in range(SIZE):
+                _s = self.board.get_at((x+1, y+1))
+                _center_x = x * CELL_W + CELL_W / 2
+                _center_y = y * CELL_H + CELL_H / 2
+                if _s == stone.BLACK:
+                    pygame.draw.circle(self.screen, COL_BLACK, (_center_x, _center_y), STONE_SIZE)
+                elif _s == stone.WHITE:
+                    pygame.draw.circle(self.screen, COL_WHITE, (_center_x, _center_y), STONE_SIZE)
+        pygame.display.flip()
+
+    def show_pass(self):
+        pass
+
+    def show_stat(self):
+        pass
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 import stone
 from game import ConsoleGame
 from player import HumanPlayer, ComputerPlayer
+from gui import HumanGUIPlayer, GUIGame
 
 
 def parse_args():
@@ -13,14 +14,18 @@ def parse_args():
     parser.add_argument("-p", "--use_process", action="store_true",
                         help="Specify when to use ProcessPoolExecutor instead of ThreadPoolExecutor.")
     parser.add_argument("-l", "--level", type=int, default=5, help="Specify the level of computer player.")
+    parser.add_argument("-g", "--use_gui", action="store_true", help="Use GUI.")
     return parser.parse_args()
 
 
 def run(args):
     com_stone, man_stone = (stone.BLACK, stone.WHITE) if args.computer_first else (stone.WHITE, stone.BLACK)
-    players = {com_stone: ComputerPlayer(com_stone, args.level, args.thread_num, args.use_process),
-               man_stone: HumanPlayer(man_stone)}
-    game = ConsoleGame(players)
+
+    com_player = ComputerPlayer(com_stone, args.level, args.thread_num, args.use_process)
+    man_player = HumanGUIPlayer(man_stone) if args.use_gui else HumanPlayer(man_stone)
+    players = {com_stone: com_player, man_stone: man_player}
+
+    game = GUIGame(players) if args.use_gui else ConsoleGame(players)
     game.play()
 
 
