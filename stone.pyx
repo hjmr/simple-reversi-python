b7a2fe7e845db69ab0325c2bd4a2ebb0c607908e
hjmr <2620990+hjmr@users.noreply.github.com>
2021-05-27 11:52:14 +0900

    optimized for Cython.

--- stone.pyx
+++ stone.pyx
@@ -2,6 +2,6 @@ BLACK = 1
 WHITE = 2
 
 
-def reverse(_stone):
-    _rev_stone = BLACK if _stone == WHITE else WHITE
+cpdef int reverse(int _stone):
+    cdef int _rev_stone = BLACK if _stone == WHITE else WHITE
     return _rev_stone
