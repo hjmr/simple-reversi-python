c1eeebd125e7555f797c7c7c0ebd86321ad37905
hjmr <2620990+hjmr@users.noreply.github.com>
2021-07-27 19:00:38 +0900

    remove max_search_level from general property.

--- player.py
+++ player.py
@@ -43,8 +43,8 @@ class ComputerPlayer(Player):
 
     def _show_stat(self, elapsed_time, pos, evaluation, evaluated_num):
         _time_per_eval_us = 1000 * 1000 * elapsed_time / evaluated_num
-        print("Put: {} Eval: {} (level:{} num:{} time:{:.2f}s tpe:{:.2f}us)".format(
-            self._pos2str(pos), evaluation, self.max_search_level, evaluated_num, elapsed_time, _time_per_eval_us))
+        print("Put: {} Eval: {} (num:{} time:{:.2f}s tpe:{:.2f}us)".format(
+            self._pos2str(pos), evaluation, evaluated_num, elapsed_time, _time_per_eval_us))
 
     def _pos2str(self, pos):
         int2str_tbl = "-ABCDEFGH-"
