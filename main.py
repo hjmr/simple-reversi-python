aaeebd008a6ef154bbeb729a2b35d28fb0981b82
hjmr <2620990+hjmr@users.noreply.github.com>
2021-07-29 16:42:01 +0900

    com vs com

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import argparse
 
 import stone
 from game import ConsoleGame
-from player import HumanPlayer, MinimaxThreadPlayer, McsPlayer
+from player import HumanPlayer, MinimaxPlayer, MinimaxThreadPlayer, McsPlayer
 from gui import HumanGUIPlayer, GUIGame
 
-from evaluator import PutPosEvaluator, PutPosCornerEvaluator, StoneNumEvaluator
+from evaluator import PutPosEvaluator, PutPosCornerEvaluator, StoneNumEvaluator, JinyaEvaluator
 
 
 def parse_args():
@@ -21,10 +21,12 @@ def parse_args():
 def run(args):
     com_stone, man_stone = (stone.BLACK, stone.WHITE) if args.computer_first else (stone.WHITE, stone.BLACK)
 
-    com1_player = McsPlayer("MCS", com_stone, try_num = args.level * 1000)
-    # com2_player = MinimaxThreadPlayer("PPC-211", man_stone, PutPosCornerEvaluator(2, 1, 1), StoneNumEvaluator(), args.level, args.thread_num)
-    # players = {com_stone: com1_player, man_stone: com2_player}
+    com1_player = McsPlayer("MCS", com_stone, args.level * 1000)
+    # com1_player = MinimaxThreadPlayer("PP", com_stone, PutPosEvaluator(), StoneNumEvaluator(), args.level, args.thread_num)
+    # com1_player = MinimaxThreadPlayer("PPC-212", com_stone, PutPosCornerEvaluator(2,1,2), StoneNumEvaluator(), args.level, args.thread_num)
+    # com2_player = MinimaxThreadPlayer("Jinya", man_stone, JinyaEvaluator(), JinyaEvaluator(), args.level, args.thread_num)
     man_player = HumanGUIPlayer(man_stone) if args.use_gui else HumanPlayer(man_stone)
+    # players = {com_stone: com1_player, man_stone: com2_player}
     players = {com_stone: com1_player, man_stone: man_player}
 
     game = GUIGame(players) if args.use_gui else ConsoleGame(players)
